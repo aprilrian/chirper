f27eedc32719e6286acbb7073427218292e40880
Aprilyanto Siburian <yantosetiyawan550@gmail.com>
2024-01-28 17:08:06 +0700

    final

---
+++ app/Http/Controllers/ChirpController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Chirp;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\ChirpRequest;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+use Inertia\Response;
+
+class ChirpController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(): Response
+    {
+        return Inertia::render('Chirps/Index', [
+            'chirps' => Chirp::with('user:id')->latest()->get(),
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(ChirpRequest $request): RedirectResponse
+    {
+        $validated = $request->validated();
+
+        $request->user()->chirps()->create($validated);
+
+        return redirect(route('chirps.index'));
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Chirp $chirp)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Chirp $chirp)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(ChirpRequest $request, Chirp $chirp): RedirectResponse
+    {
+        $this->authorize('update', $chirp);
+
+        $validated = $request->validated();
+
+        $chirp->update($validated);
+
+        return redirect(route('chirps.index'));
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Chirp $chirp): RedirectResponse
+    {
+        $this->authorize('delete', $chirp);
+
+        $chirp->delete();
+
+        return redirect(route('chirps.index'));
+    }
+}
